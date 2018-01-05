757b01351a362eeb3b8665ccb243a4b01df18145
David Smith <david.smith@gmail.com>
2018-01-05 14:04:23 -0600

    add precompilation

--- src/LittleEndianBase128.jl
+++ src/LittleEndianBase128.jl
@@ -23,6 +23,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+__precompile__()
+
 module LittleEndianBase128
 
 export encode, decodesigned, decodeunsigned, decode
